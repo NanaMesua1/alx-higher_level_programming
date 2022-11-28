a8930d66d745179a4fd9d1dd63a76d73a0773954
NanaMesua1 <dorindant17@gmail.com>
2022-11-28 12:29:30 +0000

    python shell

---
+++ 0x00-python-hello_world/3-print_number.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+number = 98
+txt = "{} Battery street"
+print(txt.format(number))
---
+++ 0x00-python-hello_world/4-print_float.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+number = 3.14159
+txt = "{:.2f}"
+print(txt.format(number))
