00e05b1f0c9bcf13eee147f2d56c783da6f1db71
NanaMesua1 <dorindant17@gmail.com>
2022-12-01 19:25:12 +0000

    python modules and imports

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """Print all names defined by hidden_4 module."""
+    import sys
+    import hidden_4
+
+
+    for n in dir(hidden_4):
+        if n[:2] != "__":
+            print(n)
