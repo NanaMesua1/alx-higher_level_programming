568afaec7ab7f3595d84b83ffbb234dd14b0045c
NanaMesua1 <dorindant17@gmail.com>
2022-12-01 19:38:28 +0000

    ython imports and modules

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,8 +3,6 @@ if __name__ == "__main__":
     """Print all names defined by hidden_4 module."""
     import sys
     import hidden_4
-
-
     for n in dir(hidden_4):
         if n[:2] != "__":
             print(n)
