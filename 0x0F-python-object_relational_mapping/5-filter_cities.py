fcb18a8a645566bc2d569e48494f86fd6fe0bf2e
Kathryn Agyeman <117659201+NanaMesua1@users.noreply.github.com>
2023-03-31 15:42:57 +0000

    Update 5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-# Displays all cities of a given state from the
-# states table of the database hbtn_0e_4_usa.
+"""
+Displays all cities of a given state from the
+states table of the database hbtn_0e_4_usa.
+"""
 
 
 import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
-    """
-    Access to the database and get the cities
-    from the database.
-    """
-
     db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
                          passwd=argv[2], db=argv[3])
 
