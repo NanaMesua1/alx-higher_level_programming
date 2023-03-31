e065be1e4dc50997df1b7cffeb6635847f433173
Kathryn Agyeman <117659201+NanaMesua1@users.noreply.github.com>
2023-03-31 15:36:17 +0000

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,14 +2,10 @@
 """Script that takes in an argument and displays all values in the states
 table of hbtn_0e_0_usa where name matches the argument"""
 
-import sys
+from sys import argv
 import MySQLdb
 
 if __name__ == '__main__':
-    """
-    Access to the database and get the states
-    from the database.
-    """
     db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
                          passwd=argv[2], db=argv[3])
 
