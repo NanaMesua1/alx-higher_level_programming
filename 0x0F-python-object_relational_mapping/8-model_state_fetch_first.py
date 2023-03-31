bc4e750f802a9c43e0d87246b7a5bdd5e595aa84
Kathryn Agyeman <117659201+NanaMesua1@users.noreply.github.com>
2023-03-31 16:00:00 +0000

    orm

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -20,6 +20,6 @@ if __name__ == "__main__":
     first_state = session.query(State).order_by(State.id).first()
     
     if first_state:
-        print("{}: {}".format(first_state.id, first_state.name))
+        print("{0}: {1}".format(first_state.id, first_state.name))
     else:
         print("Nothing")
