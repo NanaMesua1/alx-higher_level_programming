c3082a6b7ff75807cbed7f625ceea07f20aab198
Kathryn Agyeman <117659201+NanaMesua1@users.noreply.github.com>
2023-03-31 15:45:00 +0000

    orm

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Prints the first State object from the database hbtn_0e_6_usa.
+"""Prints the first State object from the database hbtn_0e_6_usa"""
 
 from sys import argv
 from model_state import Base, State
@@ -7,11 +7,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    """
-    Access to the database and get a state
-    from the database.
-    """
-
     db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         argv[1], argv[2], argv[3])
     engine = create_engine(db_uri)
