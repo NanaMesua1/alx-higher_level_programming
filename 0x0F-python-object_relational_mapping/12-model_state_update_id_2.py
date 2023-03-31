fbc19d31efc40e8f071d03d4c71d799b5b8a1c88
Kathryn Agyeman <117659201+NanaMesua1@users.noreply.github.com>
2023-03-31 16:08:40 +0000

    orm

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Changes the name of the State object with id = 2 to
+"""Changes the name of the State object with id = 2 New Mexico"""
 
 from sys import argv
 from model_state import Base, State
@@ -7,9 +7,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    """
-    Updates a State object on the database.
-    """
 
     db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         argv[1], argv[2], argv[3])
