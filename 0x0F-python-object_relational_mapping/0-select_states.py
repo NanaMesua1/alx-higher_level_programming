50ead2ae3d595ff8580bb53e97aa9e47c952996f
Kathryn Agyeman <117659201+NanaMesua1@users.noreply.github.com>
2023-03-31 15:17:46 +0000

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-# Lists all states from the database hbtn_0e_0_usa.
+"""Script that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument"""
 
+import sys
 import MySQLdb
-from sys import argv
 
 if __name__ == '__main__':
-    """
-    Access to the database and get the states
-    from the database.
-    """
-    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
-                         passwd=argv[2], db=argv[3])
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    search = sys.argv[4]
 
+    db = MySQLdb.connect(user=username, passwd=password, db=database)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states")
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY id ASC", (search,))
     rows = cur.fetchall()
-
     for row in rows:
         print(row)
+    cur.close()
+    db.close()
