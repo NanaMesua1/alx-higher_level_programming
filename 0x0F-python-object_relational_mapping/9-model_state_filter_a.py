37cd2eec5bc7cad23f262f964fa703cfd30fbca9
Kathryn Agyeman <117659201+NanaMesua1@users.noreply.github.com>
2023-03-31 16:01:30 +0000

    orm

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-# Lists all State objects that contain the letter a
-# from the database hbtn_0e_6_usa.
+"""
+Lists all State objects that contain the letter a
+from the database hbtn_0e_6_usa.
+"""
 
 from sys import argv
 from model_state import Base, State
@@ -8,10 +10,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    """
-    Access to the database and get a state
-    from the database.
-    """
 
     db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         argv[1], argv[2], argv[3])
