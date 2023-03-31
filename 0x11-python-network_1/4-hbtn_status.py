549866ccc7a142f8bf9486fcef3b66669f03ec84
Kathryn Agyeman <117659201+NanaMesua1@users.noreply.github.com>
2023-03-31 13:52:12 +0000

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status."""
 import requests
 
 
 if __name__ == "__main__":
    url = "https://alx-intranet.hbtn.io/status"
-    print("Body response:")
-    print("\t- type: {}".format(type(url.text)))
-    print("\t- content: {}".format(url.text))
+   print("Body response:")
+   print("\t- type: {}".format(type(url.text)))
+   print("\t- content: {}".format(url.text))
