dd51b1a0a4fa04b149820d81e812f807b1d76ff4
Kathryn Agyeman <117659201+NanaMesua1@users.noreply.github.com>
2023-03-31 15:33:16 +0000

    Update 1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-# Lists all states with a name starting with N from the database hbtn_0e_0_usa.
+
+"""Lists all states with a name starting with N from the database hbtn_0e_0_usa. """
 
 import MySQLdb
 from sys import argv
@@ -16,10 +17,9 @@ if __name__ == '__main__':
     cur.execute("SELECT * FROM states \
                  WHERE name LIKE BINARY 'N%' \
                  ORDER BY states.id ASC")
+    
     rows = cur.fetchall()
 
     for row in rows:
         print(row)
         
-    cur.close()
-    db.close()
